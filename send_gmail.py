c6308204c043d9047de78968dd6bc72175de8e7b
Dorin Stanchescu <dorin.grigore.stanchescu@gmail.com>
2022-07-02 17:15:10 +0200

    This script creates an email and sends it to a recipient or contact list with attachement if needed.

--- send_gmail.py
+++ send_gmail.py
@@ -7,35 +7,42 @@ import mimetypes
 import getpass
 
 
-message = EmailMessage()
-attachement_path = "/Users/dorinstanchescu/Kode/email_config/picture_mail"
-sender = "dorin.grigore.stanchescu@gmail.com"
-contacts = ["stanchescudorin@gmail.com", "stanchescuroxana@gmail.com"]
-subject = "Files automation"
-body = """
-Dear all,
-These are some files.
-Kr,
-DSG
-"""
+def generate_email (sender, recipient, subject, body, attachement_path=None):
+    """This function creates an email and sends it to a recipient or contact list with attachement if needed."""
+    
+    if isinstance(recipient, list):
+        contacts = ", ".join(recipient)
+    else:
+        contacts = recipient
 
-message["From"]=sender
-message["To"]= ", ".join(contacts)
-message["Subject"] = subject
-message.set_content(body)
+    message = EmailMessage()
+    message["From"] = sender
+    message["To"] = contacts
+    message["Subject"] = subject
+    message.set_content(body)
 
-for file in os.listdir(attachement_path):
-    path_file = os.path.join(attachement_path, file)
-    mime_type, _ = mimetypes.guess_type(file)
-    mime_type, mime_subtype = mime_type.split("/", 1)
-    with open (path_file, "rb") as atp:
-        message.add_attachment(
-            atp.read(),
-            maintype=mime_type, 
-            subtype=mime_subtype, 
-            filename=file
-        )
-with smtplib.SMTP_SSL("smtp.gmail.com",465) as mail_server:
-    mail_pass = getpass.getpass("Password? ")
-    mail_server.login(sender, mail_pass)
-    mail_server.send_message(message)
+    # These lines make the attachement optional.
+    if attachement_path != None:
+        for attached_file in os.listdir(attachement_path):
+            path_file = os.path.join(attachement_path, attached_file)
+            mime_type, _ = mimetypes.guess_type(attached_file)
+            mime_type, mime_subtype = mime_type.split("/", 1)
+
+            with open (path_file, "rb") as pf:
+                message.add_attachment(
+                    pf.read(),
+                    maintype = mime_type,
+                    subtype = mime_subtype,
+                    filename = attached_file
+                )
+    return message
+
+def send_email(message):
+    """This function sends the message to the configured SMTP_SSL gmail server."""
+    with smtplib.SMTP_SSL("smtp.gmail.com",465) as mail_server:
+        mail_pass = getpass.getpass("Password? ")
+        sender = message["From"]
+        mail_server.login(sender, mail_pass)
+        mail_server.send_message(message)
+
+
